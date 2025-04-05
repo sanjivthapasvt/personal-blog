061adc47bc5f8dddde3295cd2808e43e3cad6331
sanjivthapa <thapasvt12@gmail.com>
2025-04-05 08:27:45 +0545

    commited

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = [os.getenv("ALLOWED_HOST"), "127.0.0.1"]
-CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS")
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'

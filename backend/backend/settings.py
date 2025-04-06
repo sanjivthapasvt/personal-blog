89389396bf1262151a1016780b2339199734540e
sanjivthapa <thapasvt12@gmail.com>
2025-04-06 16:09:46 +0545

    updated content

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -107,9 +107,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 
-
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
